165b3c5e1807555cb96ec9342f178f84f0b521b5
chen hao <39700009+chtrc@users.noreply.github.com>
2021-12-28 11:35:49 +0800

    bug fix (#15)
    Co-authored-by: ch <1392620585@qq.com>

--- dao/user/update.go
+++ dao/user/update.go
@@ -8,7 +8,7 @@ import (
 )
 
 func UpdateUserDetailByID(UserDetail entity.UserDetail, Id int64) error {
-	res, err := global.MYSQL_DB.DB().Exec("update user_details set nickname=?,sex=?,updatedate=?,birthday=?,address=?,motto=?,profile=? where u_id=?", UserDetail.Nickname, UserDetail.Sex, time.Now(), UserDetail.Birthday, UserDetail.Address, UserDetail.Motto, UserDetail.Profile, Id)
+	res, err := global.MYSQL_DB.DB().Exec("update user_details set nickname=?,sex=?,updatedate=?,birthday=?,address=?,motto=?,profile=? where u_id=?", UserDetail.NickName, UserDetail.Sex, time.Now(), UserDetail.Birthday, UserDetail.Address, UserDetail.Motto, UserDetail.Profile, Id)
 	if err != nil {
 		fmt.Println("Update Failed", err)
 		return err
