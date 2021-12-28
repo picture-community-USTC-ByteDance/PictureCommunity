b9209837dd1fe45d9e811a54e78b423238fa9dac
chen hao <39700009+chtrc@users.noreply.github.com>
2021-12-28 11:35:49 +0800

    bug fix (#15)
    Co-authored-by: ch <1392620585@qq.com>

--- dao/user/update.go
+++ dao/user/update.go
@@ -1,11 +1,23 @@
 package user
 
 import (
-	"picture_community/entity/db"
+	"fmt"
+	"picture_community/entity"
 	"picture_community/global"
+	"time"
 )
 
-func UpdateUserDetailByID(userDetail db.UserDetail, Id int64) error {
-	res := global.MysqlDB.Create(userDetail)
-	return res.Error
+func UpdateUserDetailByID(UserDetail entity.UserDetail, Id int64) error {
+	res, err := global.MYSQL_DB.DB().Exec("update user_details set nickname=?,sex=?,updatedate=?,birthday=?,address=?,motto=?,profile=? where u_id=?", UserDetail.NickName, UserDetail.Sex, time.Now(), UserDetail.Birthday, UserDetail.Address, UserDetail.Motto, UserDetail.Profile, Id)
+	if err != nil {
+		fmt.Println("Update Failed", err)
+		return err
+	}
+	row, err := res.RowsAffected()
+	if err != nil {
+		fmt.Println("Rows Failed", err)
+		return err
+	}
+	fmt.Println("Update succ: ", row)
+	return err
 }
