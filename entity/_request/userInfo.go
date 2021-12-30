18f872c3edc4c5d22efdff5eaa62e66e8bbee542
KarlZeng <93875101+KarlZeng@users.noreply.github.com>
2021-12-30 15:01:48 +0800

    Zjm (#18)
    * 修改1个错误
    
    * 修改bug,本地测试已通过
    
    * 修改Nickname->NickName  Sex bool->Sex int8
    将验证登录部分写成中间件Auth
    
    Co-authored-by: 我都想笑了 <10147937+wdxxlhh@user.noreply.gitee.com>

--- entity/_request/userInfo.go
+++ entity/_request/userInfo.go
@@ -3,8 +3,8 @@ package _request
 import "time"
 
 type UserInfo struct {
-	Nickname  string    `json:"nickname"`
-	Sex       bool      `json:"sex"`
+	NickName  string    `json:"nickname"`
+	Sex       int8      `json:"sex"`
 	Motto     string    `json:"motto"`
 	Email     string    `json:"email"`
 	Telephone int       `json:"telephone"`
