0bd4acd9bf5681aeb28ee2e8d2761f15e17e26c4
July1101 <tanhuigen1101@mail.ustc.edu.cn>
2021-12-30 17:40:16 +0800

    update file

--- response/Response.go
+++ response/Response.go
@@ -6,24 +6,28 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func Response(ctx *gin.Context, httpStatus int, code int, data gin.H, msg string) {
+func Response(ctx *gin.Context, httpStatus int, code int, data interface{}, msg string) {
 	ctx.JSON(httpStatus, gin.H{"code": code, "data": data, "msg": msg})
 }
 
-func Success(ctx *gin.Context, data gin.H, msg string) {
+func Success(ctx *gin.Context, data interface{}, msg string) {
 	Response(ctx, http.StatusOK, SuccessCode, data, msg)
 }
 
-func Fail(ctx *gin.Context, data gin.H, msg string) {
+func Fail(ctx *gin.Context, data interface{}, msg string) {
 	Response(ctx, http.StatusBadRequest, FailCode, data, msg)
 }
 
-func CheckFail(ctx *gin.Context, data gin.H, msg string) {
+func CheckFail(ctx *gin.Context, data interface{}, msg string) {
 	Response(ctx, http.StatusUnprocessableEntity, CheckFailCode, data, msg)
 }
 
-func ServerError(ctx *gin.Context, data gin.H, msg string) {
+func ServerError(ctx *gin.Context, data interface{}, msg string) {
 	Response(ctx, http.StatusInternalServerError, ServerErrorCode, data, msg)
+}
+
+func UnAuthorization(ctx *gin.Context, data interface{}, msg string) {
+	Response(ctx, http.StatusInternalServerError, UnAuthorized, data, msg)
 }
 
 /*********************************************************
