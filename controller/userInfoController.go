5b54a52444ff4af0327e1b1066e5643e33855a13
ElliotFu <30978015+ElliotFu@users.noreply.github.com>
2022-01-25 00:47:19 +0800

    Fzh (#42)
     添加数据库时区设置、修复生日更新Bug

--- controller/userInfoController.go
+++ controller/userInfoController.go
@@ -1,20 +1,45 @@
 package controller
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"picture_community/entity/_request"
 	"picture_community/response"
 	"picture_community/service"
+	"time"
 )
+
+const TIME_LAYOUT = "2006-01-02"
+
+var loc *time.Location
+
+func init() {
+	var err error
+	loc, err = time.LoadLocation("Asia/Shanghai")
+	if err != nil {
+		fmt.Println(err)
+	}
+}
 
 func UpdateUserDetailController(c *gin.Context) {
 	var u _request.UpdateUserDetailInfo
-
+	var birthdayTime time.Time
 	if err := c.ShouldBind(&u); err != nil {
 		response.CheckFail(c, nil, "更新用户信息参数错误")
+		return
+	}
+	if u.Birthday != "" {
+		var err error
+		birthdayTime, err = time.ParseInLocation(TIME_LAYOUT, u.Birthday, loc)
+		if err != nil {
+			fmt.Println(err)
+			response.CheckFail(c, nil, "更新用户信息参数错误")
+			return
+		}
 	}
 	uid, _ := c.Get("uid")
-	isOK, message := service.UpdateUserDetailService(u, uid.(uint))
+	fmt.Println(birthdayTime)
+	isOK, message := service.UpdateUserDetailService(u, birthdayTime, uid.(uint))
 	if isOK {
 		response.Success(c, nil, message)
 	} else {
--- global/global.go
+++ global/global.go
@@ -7,9 +7,9 @@ import (
 )
 
 var (
-	//DbUrl   = "root:root@(127.0.0.1:3306)/test3?charset=utf8mb4&parseTime=True"
-	//DbUrl = "root:FZHZYwade@(127.0.0.1:3306)/test?charset=utf8mb4&parseTime=True"
-	DbUrl   = "root:123456@(121.5.1.73:3306)/pic?charset=utf8mb4&parseTime=True"
+	//DbUrl   = "root:root@(127.0.0.1:3306)/test3?charset=utf8mb4&parseTime=True&loc=Local"
+	//DbUrl = "root:FZHZYwade@(127.0.0.1:3306)/test?charset=utf8mb4&parseTime=True&loc=Local"
+	DbUrl   = "root:123456@(121.5.1.73:3306)/pic?charset=utf8mb4&parseTime=True&loc=Local"
 	MysqlDB *gorm.DB
 
 	GinEngine *gin.Engine
