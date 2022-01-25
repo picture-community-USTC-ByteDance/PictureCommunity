dd783a012bbfe7a961092b6cbd5916f77831f66d
DrinkHotWater <34021412+DrinkHotWater@users.noreply.github.com>
2022-01-25 09:07:10 +0800

    Zhangheng (#36)
    * test
    
    * 完成用户主页个人信息和帖子信息的查询
    
    * 完成用户主页个人信息和帖子信息的查询
    
    * 完成用户主页个人信息和帖子信息的查询
    
    * 完成用户主页个人信息api 和 帖子数组查询api
    
    * 用户主页基本信息查询+帖子数组查询

---
+++ dao/user/query.go
@@ -0,0 +1,31 @@
+package user
+
+import (
+	"picture_community/entity/_response"
+	"picture_community/entity/db"
+	"picture_community/global"
+)
+
+// 在用户主页，查自己的用户信息
+func QueryUserDataByUserID(uid uint) (error, _response.UserData) {
+	var userdata _response.UserData
+	// 在db.UserData{}数据库中查询，查询结果保存到结构体变量userdata中
+	err := global.MysqlDB.Model(db.UserData{}).
+		Select("nickname,sex,profile,motto,followers_number,fans_number,posts_number,collection_number,forward_number").
+		Joins("inner join user_detail on user_data.uid = user_detail.uid").
+		Where("uid=?", uid).Scan(&userdata).Error
+
+	return err, userdata
+}
+
+// 在用户主页，查自己的帖子数组信息
+func QueryUserPostsByUserID(uid uint, page int, pageSize int) (error, int64, []_response.UserPosts) {
+	var userposts []_response.UserPosts
+	var count int64
+	// 在db.Post{}数据库中查询，查询结果保存到结构体变量userposts中
+	err := global.MysqlDB.Model(db.Post{}).
+		Select("pid,title_photo_url,like_number,comment_number,forward_number").
+		Where("uid=?", uid).Count(&count).
+		Offset((page - 1) * pageSize).Limit(pageSize).Scan(userposts).Error
+	return err, count, userposts
+}
