26b46326ae477991d683d60db9a88f237bc8340e
73610 <1>
2022-02-20 11:53:04 +0800

    对于获取评论返回值进行修改 堆数据库评论表添加了昵称头像字段 创建评论的参数添加了部分数据

--- entity/_request/Comment.go
+++ entity/_request/Comment.go
@@ -1,15 +1,19 @@
 package _request
 
 type CreateFirstLevelComment struct {
-	PID     uint   `form:"p_id" json:"p_id" binding:"required"`
-	UID     uint   `form:"u_id" json:"u_id" binding:"required"`
-	Content string `form:"content" json:"content" binding:"required"`
+	PID      uint   `form:"p_id" json:"p_id" binding:"required"`
+	UserID   uint   `form:"u_id" json:"u_id" binding:"required"`
+	Content  string `form:"content" json:"content" binding:"required"`
+	Nickname string `form:"nickname" json:"nickename"`
+	Profile  string `form:"profile" json:"profile"`
 }
 type CreateSecondLevelComment struct {
 	PID      uint   `form:"p_id" json:"p_id" binding:"required"`
-	UID      uint   `form:"u_id" json:"u_id" binding:"required"`
+	UserID   uint   `form:"u_id" json:"u_id" binding:"required"`
 	ParentId uint   `form:"parent_id" json:"parent_id" binding:"required"`
 	Content  string `form:"content" json:"content" binding:"required"`
+	Nickname string `form:"nickname" json:"nickename"`
+	Profile  string `form:"profile" json:"profile"`
 }
 type DeleteComment struct {
 	CID uint `form:"c_id" json:"c_id" binding:"required"`
@@ -17,15 +21,14 @@ type DeleteComment struct {
 	//ParentId uint 		`form:"parent_id" json:"parent_id" binding:"required"`
 }
 type QueryComment struct {
-	PID      int `form:"p_id" json:"p_id" binding:"required"`
-	PageSize int `form:"pagesize"  binding:"required"`
-	Page     int `form:"page"  binding:"required"`
+	PID      uint `form:"p_id" json:"p_id" binding:"required"` //帖子id
+	PageSize int  `form:"pagesize"  binding:"required"`
+	Page     int  `form:"page"  binding:"required"`
 }
 
 type QueryComment2 struct {
-	PID      int `form:"p_id" json:"p_id" binding:"required"`
-	ParentId int `form:"parentid" json:"parentid"`
-	PageSize int `form:"pagesize"  binding:"required"`
-	Page     int `form:"page"  binding:"required"`
+	PID      uint `form:"p_id" json:"p_id" binding:"required"`
+	ParentId uint `form:"parent_id" json:"parent_id" binding:"required"`
+	PageSize int  `form:"pagesize"  binding:"required"`
+	Page     int  `form:"page"  binding:"required"`
 }
-
--- entity/_response/Comment.go
+++ entity/_response/Comment.go
@@ -1,25 +1,37 @@
 package _response
 
-type QueryCommentBack struct {
+import "time"
 
-	UID      int    `json:"userId"`
+type QueryCommentBack struct {
+	UserID   int    `json:"userId"`
 	NickName string `json:"nickName"`
 	//	Profile     string //头像略缩图
 	ChildNumber int    //子评论个数
 	Content     string //内容
 }
 type QueryCommentBackTemp struct {
-
-	ChildNumber int //子评论个数
-	LikeNumber  int
-	Content     string //内容
-	UserID      uint   //评论作者id
+	CID         uint `form:"c_id" json:"c_id" binding:"required"` //评论id
+	UserID      uint //(评论)用户id
+	NickName    string
+	Profile     string    //略缩图
+	Content     string    //内容
+	UpdateTime  time.Time //评论时间
+	LikeNumber  uint      //评论的点赞数
+	ChildNumber int       //子评论个数
+	LikeStatus  bool      //true表示已点赞
 }
 type QueryCommentBackTemp2 struct {
-	//	ChildNumber int    //子评论个数
-	LikeNumber int
-	Content    string //内容
-	UserID     uint   //评论作者id
+	CID uint `form:"c_id" json:"c_id" binding:"required"` //评论id
+
+	UserID   uint //(评论)用户id
+	NickName string
+	Profile  string //略缩图
+
+	Content    string    //内容
+	UpdateTime time.Time //评论时间
+	LikeNumber uint      //评论的点赞数
+	LikeStatus bool      //true表示已点赞
+	ParentId   uint      `form:"parentid" json:"parentid"`
 }
 type CreateFirstLevelCommentBack struct {
 	PID     uint   `json:"post_id"`
