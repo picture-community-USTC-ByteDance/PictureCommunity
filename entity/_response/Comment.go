e6f9fe820750a805a633a1b2d89c97880c3aa43c
smil-e <54132244+smil-e@users.noreply.github.com>
2022-03-03 15:31:05 +0800

    Chenqing2 (#87)
    * 评论部分
    增加一级评论
    增加二级评论
    删除评论
    返回评论
    
    * 添加一个查询二级评论的接口
    完善一些代码
    对comment表加了两个表项：点赞数和删除状态
    
    * 对于获取评论返回值进行修改
    堆数据库评论表添加了昵称头像字段
    创建评论的参数添加了部分数据
    
    * 修复了一些bug
    对之前的代码上的一些不足，进行了完善，修改了插入评论时可能会引起的不一致情况。
    修改了新增评论的接口参数
    修改了新增评论后的返回参数
    
    * 修复了一些bug
    对之前的代码上的一些不足，进行了完善，修改了插入评论时可能会引起的不一致情况。
    修改了新增评论的接口参数
    修改了新增评论后的返回参数
    
    * 返回评论的参数中增加了username
    
    * 修改cmment表，加了DAO函数，修改了service层
    
    * 修改了创建评论的返回信息
    
    Co-authored-by: 73610 <1>

--- entity/_response/Comment.go
+++ entity/_response/Comment.go
@@ -1,7 +1,6 @@
 package _response
 
 import (
-	"picture_community/entity/db"
 	"time"
 )
 
@@ -28,11 +27,22 @@ type QueryCommentBack2 struct {
 	LikeNumber uint      //评论的点赞数
 	ParentId   uint
 	Username   string
-
 }
 
-type CreateFirstLevelCommentBack = db.Comment
-type CreateSecondLevelCommentBack = db.Comment
+type CreateCommentBack struct {
+	CID          uint `gorm:"primaryKey"`
+	PostID       uint
+	ParentID     uint      //如果为一级评论则为空
+	ChildNumber  int       //子评论个数
+	Content      string    //内容
+	UserID       uint      //评论作者id
+	LikeNumber   uint      //评论的点赞数
+	DeleteStatus bool      //评论是否被删除 true表示删除
+	UpdateTime   time.Time `gorm:"autoUpdateTime:milli"`
+	CreateTime   time.Time `gorm:"autoCreateTime:milli"`
+	NickName     string
+	Profile      string //略缩图
+}
 
 //type DeleteCommentBack struct {
 //	PID 		uint	`json:"post_id"`
